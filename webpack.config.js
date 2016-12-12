c48b200fcd011cf5e41bf78f802598c82c00152e
Aleksandr Kalinin <8kalinin@gmail.com>
2016-12-12 14:07:35 +0500

    Refactored webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,54 @@
 'use strict';
 
-const devEnv = process.env.NODE_ENV == 'development';
+const isProduction = process.env.NODE_ENV == 'production';
 
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const AssetsPlugin = require('assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let entry = {
+  app: ['./app']
+};
+
+let loaders = [
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /\/node_modules\//,
+    query: {
+      plugins: ["transform-decorators-legacy"],
+      presets: ['es2015', 'react', 'stage-0']
+    }
+  }
+  // {
+  //   test: /\.css$/,
+  //   loader: 'style!css'
+  // }, {
+  //   test: /\.(ttf|eot|woff|woff2)$/,
+  //   loader: isProduction ? 'file?name=fonts/[hash].[ext]' : 'file?name=fonts/[name].[ext]?[hash]'
+  // }, {
+  //   test: /\.(png|jpg|svg|gif)$/,
+  //   loader: isProduction ? 'file?name=img/[hash].[ext]' : 'file?name=img/[name].[ext]?[hash]'
+  // }
+];
+
+let plugins = [
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+  }),
+  new HtmlWebpackPlugin({
+    template: '../index.html',
+    minify: {
+      removeComments: isProduction,
+      collapseWhitespace: isProduction
+    },
+    inject: true
+  }),
+  new webpack.ProvidePlugin({
+    'React': 'react'
+  })
+];
 
 const devPlugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
@@ -17,47 +61,49 @@ const prodPlugins = [
       drop_console: true,
       unsafe: true
     }
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'common',
+    minChunks: 3
   })
 ];
+
+if (isProduction) {
+  plugins.unshift(
+    new CleanWebpackPlugin(['build'], {
+      root: __dirname,
+      verbose: true
+    })
+  );
+  plugins.push(...prodPlugins);
+} else {
+  plugins.push(...devPlugins);
+  entry.app.unshift('webpack-hot-middleware/client', 'react-hot-loader/patch')
+}
 
 module.exports = {
   context: __dirname + '/frontend',
 
-  entry: {
-    app: ['./app']
-  },
+  entry: entry,
 
   output: {
     path: __dirname + '/build',
     publicPath: '/',
-    filename: devEnv ? '[name].js' : '[name].[chunkhash].js'
+    filename: isProduction ? '[name].[chunkhash].js' : '[name].js'
   },
 
-  watch: devEnv,
+  watch: !isProduction,
 
   watchOptions: {
     aggregateTimeout: 100
   },
 
-  devtool: devEnv ? 'inline-source-map' : null,
+  devtool: isProduction ? null : 'inline-source-map',
 
-  plugins: [
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      minChunks: 3
-    }),
-    new AssetsPlugin({
-      filename: 'assets.json',
-      path: __dirname + '/build'
-    })
-  ],
+  plugins: plugins,
 
   resolve: {
-    modulesDirectories: ['node_modules'],
+    modulesDirectories: ['node_modules', './components', './containers'],
     extensions: ['', '.js']
   },
 
@@ -68,40 +114,6 @@ module.exports = {
   },
 
   module: {
-    loaders: [
-      {
-        test: /\.(njk|nunjucks)$/,
-        loader: 'nunjucks-loader'
-      }, {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /\/node_modules\//,
-        query: {
-          presets: ['es2015']
-        }
-      }, {
-        test: /\.css$/,
-        loader: 'style!css'
-      }, {
-        test: /\.(ttf|eot|woff|woff2)$/,
-        loader: devEnv ? 'file?name=fonts/[name].[ext]?[hash]' : 'file?name=fonts/[hash].[ext]'
-      }, {
-        test: /\.(png|jpg|svg|gif)$/,
-        loader: devEnv ? 'file?name=img/[name].[ext]?[hash]' : 'file?name=img/[hash].[ext]'
-      }
-    ]
+    loaders: loaders
   }
 };
-
-if (!devEnv) {
-  module.exports.plugins.unshift(
-    new CleanWebpackPlugin(['build'], {
-      root: __dirname,
-      verbose: true
-    })
-  );
-  module.exports.plugins.push(...prodPlugins);
-} else {
-  module.exports.plugins.push(...devPlugins);
-  module.exports.entry.app.unshift('webpack-hot-middleware/client')
-}
